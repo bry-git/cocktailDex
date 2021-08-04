a895dbc73bbc9127adb967c99086998d96359006
bryson crandall <bryson.crandall@yahoo.com>
2021-08-04 07:21:46 -0700

    navbar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
+
 import './App.css';
+import './components/NavBar'
+import Navbar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
-     
+     <Navbar/>
      
     </div>
   );
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import InputBase from '@material-ui/core/InputBase';
+import SearchIcon from '@material-ui/icons/Search';
+import { StylesProvider } from "@material-ui/core/styles";
+import useStyles from './NavBar.styles'
+import './NavBar.css'
+
+const Navbar = () => {
+    const classes = useStyles();
+  
+    return (
+     <StylesProvider injectFirst>  
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+
+          <img src="../icons/cocktail-solid(1).svg"></img>
+
+            <Typography className={classes.title} variant="h6" noWrap>
+              CocktailDex
+            </Typography>
+
+
+            <div className={classes.search}>
+              <div className={classes.searchIcon}>
+                <SearchIcon />
+              </div>
+              <InputBase
+                placeholder="Search…"
+                classes={{
+                  root: classes.inputRoot,
+                  input: classes.inputInput,
+                }}
+                inputProps={{ 'aria-label': 'search' }}
+              />
+            </div>
+          </Toolbar>
+        </AppBar>
+      </div>
+    </StylesProvider> 
+    );
+  }
+
+  export default Navbar
