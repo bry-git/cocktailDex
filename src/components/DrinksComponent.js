b2e0fdcdc31f2c0199fd68bc96a31da44e7b5d9d
Evan Fillman <efillman@gmail.com>
2021-08-05 11:50:57 -0400

    inital search bar working

--- src/components/DrinksComponent.js
+++ src/components/DrinksComponent.js
@@ -10,10 +10,20 @@ const DrinksComponent = (props) => {
   const [data, setData] = useState([])
   const [isLoading, setIsLoading] = useState(true)
 
+  //this loads getDrinksPopular (the default load)
   useEffect(() => {
     const dataHandler = new DataHandlerComponent();
     dataHandler.getDrinksPopular().then((data) => setData(data)).then(() => setIsLoading(false))
   }, []);
+
+  useEffect(() => {
+    const searchQuery = props.searchQuery;
+    if (searchQuery) {
+      const dataHandler = new DataHandlerComponent();
+      dataHandler.getDrinksBySearch(0,50,props.searchQuery).then((data) => setData(data)).then(() => setIsLoading(false))
+    }
+  }, [props.searchQuery]);
+
 
   return (
     <>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,16 +1,26 @@
-import React from 'react';
+import  React, {useState} from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import InputBase from '@material-ui/core/InputBase';
 import SearchIcon from '@material-ui/icons/Search';
+import IconButton from '@material-ui/core/IconButton';
+import Button from '@material-ui/core/Button';
 import { StylesProvider } from "@material-ui/core/styles";
 import { Link } from "react-router-dom";
 import useStyles from './NavBar.styles'
 import './NavBar.css'
 
-const Navbar = () => {
+const Navbar = (props) => {
+  const [searchQuery, setSearchQuery] = useState('');
   const classes = useStyles();
+  const handleSearchInputChange = (event) => {
+    //console.log('Search', event.target.value);
+    setSearchQuery(event.target.value);
+  }
+  const handleSearchClick = () => {
+    props.setSearchCallback(searchQuery);
+  }
 
   return (
     <StylesProvider injectFirst>
@@ -36,10 +46,13 @@ const Navbar = () => {
               </Link>
             </Typography> */}
 
-
+            <IconButton onClick={handleSearchClick}>
+                    <SearchIcon />
+            </IconButton>
             <div className={classes.search}>
               <div className={classes.searchIcon}>
-                <SearchIcon />
+                {/*TODO: fix this search button in the search bar something really wrong*/}
+                    <SearchIcon />
               </div>
               <InputBase
                 placeholder="Search…"
@@ -47,6 +60,8 @@ const Navbar = () => {
                   root: classes.inputRoot,
                   input: classes.inputInput,
                 }}
+                onChange={handleSearchInputChange}
+                value={searchQuery}
                 inputProps={{ 'aria-label': 'search' }}
               />
             </div>
